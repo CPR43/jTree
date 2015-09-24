a82c7113b22ddfa847d5741504f3db3d55215838
CPR43 <prao@SUYPC115.ASI.local>
2015-09-24 12:27:37 +0530

    intial js
    Its the main js that contains the logic for creating and arranging the
    file

---
+++ jtree-1.0.0.js
@@ -0,0 +1,58 @@
+$.fn.folderTree = function(object) {
+    var ul = $("<ul/>", {
+        "class": "tree_view"
+    });
+    parseChild(ul, object[0].Subtree);
+    this.append(ul);
+    rearrange();
+
+};
+$("li").click(function(e) {
+    e.stopPropagation();
+    $(this).children("ul").slideToggle();
+});
+
+function parseChild(htmlObj, parent) {
+    $.each(parent, function(i, val) {
+        var sub = $("<li/>");
+        var anchor = $("<a/>", {
+            "class": ""
+        });
+        var italic = $("<i/>", {
+            "class": "fa fa-folder-open-o"
+        });
+        var italics_file = $("<i/>", {
+            "class": "fa fa-file"
+        });
+        sub.append(anchor);
+        if (val.Subtree != null) {
+            anchor.append(italic);
+        } else {
+            anchor.append(italics_file);
+        }
+        anchor.append($("<span/>", {
+            text: val.name
+        }));
+        htmlObj.append(sub);
+        if (val.Subtree != null) {
+            var subUl = $("<ul/>", {
+                "class": "subUl"
+            });
+            sub.append(subUl);
+            parseChild(subUl, val.Subtree);
+        }
+    });
+}
+
+function rearrange() {
+    $('li ul').each(function(){
+        var temp = 0;
+        var ul = $(this);
+        $(this).children('li').each(function() {
+            if ($(this).children('ul').length > 0) {
+                $(this).prependTo(ul);
+            };
+        });
+        ul.slideToggle();
+    });
+}
